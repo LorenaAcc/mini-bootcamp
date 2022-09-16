ddf86247c6ee189ff57263195ec3579b87000cab
lorenamartin <martin.lorenasofia@gmail.com>
2022-09-16 17:35:32 -0300

    updating HttpRequests

--- Training/HttpRequests/src/app/posts.service.ts
+++ Training/HttpRequests/src/app/posts.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject, throwError } from 'rxjs';
 import { Post } from './post.model';
-import { map, catchError } from 'rxjs';
+import { map, catchError, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
@@ -17,7 +17,11 @@ export class PostsService{
         this.http
         .post<{ name: string }>(
             'https://recipe-book-8617d-default-rtdb.firebaseio.com/posts.json', 
-            postData
+            postData,
+            {
+                //observe: 'body'
+                observe: 'response'
+            }
         )
         //I subscribe here because the component does not care about the response
         .subscribe(responseData => {
@@ -28,9 +32,22 @@ export class PostsService{
     }
 
     fetchPosts(){
+      let searchParams = new HttpParams();
+      searchParams = searchParams.append('print','pretty');
+      searchParams = searchParams.append('custom','key');
       //return the observable
       return this.http
-      .get<{ [ key: string ] : Post }>('https://recipe-book-8617d-default-rtdb.firebaseio.com/posts.json')
+      .get<{ [ key: string ] : Post }>('https://recipe-book-8617d-default-rtdb.firebaseio.com/posts.json',
+      {
+        headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+        //params: new HttpParams().set('print', 'pretty')
+        params: searchParams,
+        // with that: .get<{ [ key: string ] : Post }>('https://recipe-book-8617d-default-rtdb.firebaseio.com/posts.json',
+        //I can not put : responseType: 'text'
+        //because '.get' already has an assignment of type object and not text
+        responseType: 'json'
+      }
+      )
       .pipe(
         map(responseData => {
         const postsArray: Post[] = [];
@@ -49,6 +66,23 @@ export class PostsService{
     }
 
     deletePosts(){
-      return this.http.delete('https://recipe-book-8617d-default-rtdb.firebaseio.com/posts.json');
+      return this.http.delete('https://recipe-book-8617d-default-rtdb.firebaseio.com/posts.json',
+      {
+        //It extracts the response body and convert it to javascript:
+        //observe: 'body'
+        observe: 'events',
+        //responseType: 'json'
+        responseType: 'text'
+      })
+      .pipe(tap(event =>{
+        console.log(event);
+        if(event.type === HttpEventType.Sent){
+            //......
+        }
+        if(event.type === HttpEventType.Response){
+            console.log(event.body);
+        }
+      })
+      );
     }
 }
