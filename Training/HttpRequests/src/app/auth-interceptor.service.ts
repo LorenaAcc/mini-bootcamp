ffa5f46059548344c3352f094b27b4200ba05573
lorenamartin <martin.lorenasofia@gmail.com>
2022-09-19 10:05:24 -0300

    Adding logging interceptor

--- Training/HttpRequests/src/app/auth-interceptor.service.ts
+++ Training/HttpRequests/src/app/auth-interceptor.service.ts
@@ -1,8 +1,22 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 export class AuthInterceptorService implements HttpInterceptor{
     intercept(req: HttpRequest<any>, next: HttpHandler){
-        console.log('Request is on its way');
-        return next.handle(req);
-    }
+        // console.log('Request is on its way');
+        // console.log(req.url);
+        const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
+        return next.handle(modifiedRequest);
+    //     return next.handle(modifiedRequest).pipe(
+    //         tap(event => {
+    //             console.log(event);
+    //             if(event.type === HttpEventType.Response) {
+    //                 console.log('Response arrived, body data: ');
+    //                 console.log(event.body);
+    //             }
+    //         })
+    //     );
+     }
 }
+
+//Removing what is commented, now this interceptor is only attaching the auth header
