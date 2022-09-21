831beee51392fc3acd1dfb4e77ffe57c6ba73cca
lorenamartin <martin.lorenasofia@gmail.com>
2022-09-21 17:39:10 -0300

    Improving Error Handling

--- Training/Recipe_Book/src/app/auth/auth.service.ts
+++ Training/Recipe_Book/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 //Body response type
 interface AuthResponseData {
@@ -23,7 +24,22 @@ export class AuthService{
                 password:password,
                 returnSecureToken: true
             }
-        );
+        )
+        .pipe(
+            catchError(errorRes => {
+              let errorMessage = 'An unknown error occurred!';
+              if (!errorRes.error || !errorRes.error.error) {
+                return throwError(() => new Error(errorMessage));
+                //return throwError(errorMessage);
+              }
+              switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                  errorMessage = 'This email exists already';
+              }
+              return throwError(() => new Error(errorMessage));
+              //return throwError(errorMessage);
+            })
+          );
     }
 }
 
