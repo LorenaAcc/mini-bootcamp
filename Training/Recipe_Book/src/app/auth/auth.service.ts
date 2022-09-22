93752338137ed7324847b8192589965bbe81ba99
lorenamartin <martin.lorenasofia@gmail.com>
2022-09-22 12:54:18 -0300

    Fix login

--- Training/Recipe_Book/src/app/auth/auth.service.ts
+++ Training/Recipe_Book/src/app/auth/auth.service.ts
@@ -48,7 +48,7 @@ export class AuthService{
     login(email: string, password: string) {
       return this.http
         .post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBcfKMtZAEKu7spq708O2JrOuPHMlSF6Fg',
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBcfKMtZAEKu7spq708O2JrOuPHMlSF6Fg',
             {
                 email: email,
                 password: password,
