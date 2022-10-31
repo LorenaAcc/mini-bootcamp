8da8a4ea5aefdf8675266d8f62ab1402ed87dc40
lorenamartin <martin.lorenasofia@gmail.com>
2022-10-31 09:28:09 -0300

    Fix RecipeBook & new folder in NodeJs Project

---
+++ Training_NodeJs/01-hola-mundo/app.js
@@ -0,0 +1,13 @@
+let nombre = 'Lorena';
+console.log(nombre);
+
+nombre = 'Sol';
+console.log(nombre);
+
+const nombre1 = 'Sofia';
+console.log(nombre1);
+
+const nombre2 = 'Karen';
+console.log(nombre2);
+
+console.log('Hola Mundo!');
