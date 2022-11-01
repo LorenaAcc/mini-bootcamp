4d3d31314ba98506af8d3ce8f0667782540e0dad
lorenamartin <martin.lorenasofia@gmail.com>
2022-11-01 18:14:13 -0300

    Recibir informacion desde linea de comando

--- Training_NodeJs/03-bases-node/app.js
+++ Training_NodeJs/03-bases-node/app.js
@@ -2,8 +2,14 @@ const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
 
-const base = 3;
+console.log(process.argv);
+const [,,arg3 = 'base=5'] = process.argv;
+const [, base = 5] = arg3.split('=');
+console.log(base);
 
-crearArchivo( base )
-    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch(err => console.log(err));
+
+//const base = 3;
+
+// crearArchivo( base )
+//     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+//     .catch(err => console.log(err));
