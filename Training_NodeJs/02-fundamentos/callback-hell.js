5441bf57319db53f069eb1be601a79ce7c8772cb
lorenamartin <martin.lorenasofia@gmail.com>
2022-11-01 12:28:31 -0300

    Problemas comunes con los callbacks - callback-hell

---
+++ Training_NodeJs/02-fundamentos/callback-hell.js
@@ -0,0 +1,60 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: 'Lorena'
+    },
+    {
+        id: 2,
+        nombre: 'Sofia'
+    },
+    {
+        id: 3,
+        nombre: 'Karen'
+    } 
+];
+
+const salarios = [
+    {
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 1500
+    }
+];
+
+const getEmpleado = ( id ) => {
+    const empleado = empleados.find( e => e.id === id )
+
+    if(empleado) {
+        return empleado;    
+    } else {
+        return `Empleado con id ${id} no existe`
+    }
+       
+}
+
+console.log(getEmpleado ( 3 ));
+
+/////////////////////////
+const getEmpleadoConCallback = ( id, callback ) => {
+    const empleado = empleados.find( e => e.id === id )
+
+    if(empleado) {
+        callback(null, empleado);    
+    } else {
+        callback(`Empleado con id ${id} no existe`);
+    }
+       
+}
+
+getEmpleadoConCallback( 1, ( err, empleado ) =>{
+    if(err) {
+        console.log('ERROR!!')
+        return console.log(err);
+    }
+    console.log('Empleado existe!');
+    console.log(empleado);
+        
+});
