365a8e5ba01fd17a0dc2ab72696a2d12079195b9
lorenamartin <martin.lorenasofia@gmail.com>
2022-11-02 12:12:39 -0300

    yargs

--- Training_NodeJs/03-bases-node/app.js
+++ Training_NodeJs/03-bases-node/app.js
@@ -1,11 +1,25 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+//utilizando yargs - me interesa que esxtraiga del paquete de yargs: argv
+const argv = require('yargs').argv;
 
 console.clear();
 
-console.log(process.argv);
-const [,,arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
-console.log(base);
+//argv que se encuentra en los procesos propios de node
+console.log( process.argv );
+//argv que se encuentra en yargs
+console.log(argv.base);
+
+//si yo quisiera la base
+console.log('base: yargs', argv.base);
+
+
+
+
+
+// console.log(process.argv);
+// const [,,arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
+// console.log(base);
 
 
 //const base = 3;
