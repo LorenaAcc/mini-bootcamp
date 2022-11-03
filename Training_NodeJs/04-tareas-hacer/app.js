b33f89307cc1c3dfe2820122cc846d3ea36d2f1e
lorenamartin <martin.lorenasofia@gmail.com>
2022-11-03 15:13:02 -0300

    Logica para  el manejo de las tareas por hacer

--- Training_NodeJs/04-tareas-hacer/app.js
+++ Training_NodeJs/04-tareas-hacer/app.js
@@ -2,6 +2,8 @@ require('colors');
 
 //const { mostrarMenu, pausa } = require('./helpers/mensajes');
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 console.clear();
 
@@ -14,8 +16,16 @@ const main = async() => {
 
     do {
         //opt = await  mostrarMenu();
-        opt = await  inquirerMenu();
-        console.log({ opt });
+
+
+        // opt = await  inquirerMenu();
+        // console.log({ opt });
+
+        const tareas = new Tareas();
+        //const tarea = new Tarea('Comprar Comida');
+        console.log(tareas);
+
+
         await pausa();
         //if (opt !== '0') await pausa();
     } while( opt !== '0' );
