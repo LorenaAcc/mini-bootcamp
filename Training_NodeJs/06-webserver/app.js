8ff02b9a7f89dd75abd7c4fae8b7baafd54fb08e
lorenamartin <martin.lorenasofia@gmail.com>
2022-11-10 19:16:24 -0300

    Argumentos desde el controlador

--- Training_NodeJs/06-webserver/app.js
+++ Training_NodeJs/06-webserver/app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { nombre: 'Lorena Martin', titulo: 'Curso Node'});
 });
 
 app.get('/generic', (req, res) => {
