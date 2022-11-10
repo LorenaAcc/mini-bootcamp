bd338aad6d574a7478082d3bc926b84e1c08d9f2
lorenamartin <martin.lorenasofia@gmail.com>
2022-11-10 14:39:33 -0300

    Servir contenido estático

--- Training_NodeJs/06-webserver/app.js
+++ Training_NodeJs/06-webserver/app.js
@@ -2,16 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Home Page')
-});
+//Servir contenido estático
+app.use(express.static('public'));
+
+
 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hola mundo en su respectiva ruta')
 });
 
 app.get('*', (req, res) => {
-    res.send('404 | PAGE NOT FOUND')
+    res.sendFile(__dirname + '/public/404.html')
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
