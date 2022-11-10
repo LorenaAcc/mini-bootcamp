0ed1b3b9546a899966db83a71b55cf15ef2f233a
lorenamartin <martin.lorenasofia@gmail.com>
2022-11-10 12:48:47 -0300

    Introducción a Express

---
+++ Training_NodeJs/06-webserver/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Home Page')
+});
+
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola mundo en su respectiva ruta')
+});
+
+app.get('*', (req, res) => {
+    res.send('404 | PAGE NOT FOUND')
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
