caa9cd859462756d6821f1463af7053d7084e9ef
lorenamartin <martin.lorenasofia@gmail.com>
2022-11-14 12:19:40 -0300

    Parámetros de segmento y query

--- Training_NodeJs/07-restserver/controllers/usuarios.controller.js
+++ Training_NodeJs/07-restserver/controllers/usuarios.controller.js
@@ -1,9 +1,16 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = response) => {
+    const {q, nombre = 'no name', apikey, page = 1, limit} = req.query;
+
     res.json({
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
     });
 }
 
@@ -18,8 +25,11 @@ const usuariosPost = (req, res = response) => {
 }
 
 const usuariosPut = (req, res = response) => {
+    const {id} = req.params;
+
     res.status(400).json({
-        msg: 'put API - usuariosPut'
+        msg: 'put API - usuariosPut',
+        id
     });
 }
 
