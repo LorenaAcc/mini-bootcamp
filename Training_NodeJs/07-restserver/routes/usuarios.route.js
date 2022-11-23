c03e935d5b2ae0202a3217e8c867c6398b908804
Lorena Martin <lorena.martin@accenture.com>
2022-11-23 07:15:57 -0300

    Validar todos los campos

--- Training_NodeJs/07-restserver/routes/usuarios.route.js
+++ Training_NodeJs/07-restserver/routes/usuarios.route.js
@@ -1,13 +1,18 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { usuariosGet, usuariosPost, usuariosPut, usuariosPatch, usuariosDelete } = require('../controllers/usuarios.controller')
 const router = Router();
 
 
 router.get('/', usuariosGet);
-
+//si pasan los checks del middleware => valida campod
 router.post('/',[
+	check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+	check('password', 'El password es obligatorio y debe tener más de 6 caracteres').isLength({min: 6}),
 	check('correo', 'El correo no es válido').isEmail(),
+	check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+	validarCampos
 ], usuariosPost);
 
 router.put('/:id',  usuariosPut);
