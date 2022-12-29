d23083afad65e0afd258f0b360e4f874ccf021d0
Lorena Martin <lorena.martin@accenture.com>
2022-12-29 07:03:55 -0300

    Buscar en colecciones

--- Training_NodeJs/Proyecto-Node-Restserver/controllers/buscar.controller.js
+++ Training_NodeJs/Proyecto-Node-Restserver/controllers/buscar.controller.js
@@ -4,7 +4,7 @@ const { Usuario, Categoria, Producto } = require('../models');
 
 const coleccionesPermitidas = [
     'usuarios',
-    'categoria',
+    'categorias',
     'productos',
     'roles'
 ];
@@ -14,11 +14,70 @@ const buscarUsuarios = async(termino = '', res = response) => {
 
     if(esMongoId) {
         const usuario = await Usuario.findById(termino);
-        res.json({
+        return res.json({
             results: (usuario)? [usuario] : []
-        })
+        });
     }
+
+    const regex = new RegExp(termino, 'i');
+
+    const usuarios = await Usuario.find({
+        $or: [{nombre: regex}, {correo: regex}],
+        $and: [{estado: true}]
+    });
+
+    res.json({
+        resulsts: usuarios
+    });
+
 }
+
+const buscarCategorias = async(termino = '', res = response) => {
+    esMongoId = ObjectId.isValid(termino); //TRUE
+
+    if(esMongoId) {
+        const categoria = await Categoria.findById(termino);
+        return res.json({
+            results: (categoria)? [categoria] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const categorias = await Categoria.find({nombre: regex, estado: true});
+
+    res.json({
+        resulsts: categorias
+    });
+
+}
+
+const buscarProductos = async(termino = '', res = response) => {
+    esMongoId = ObjectId.isValid(termino); //TRUE
+
+    if(esMongoId) {
+        const producto = await Producto
+                                .findById(termino)
+                                .populate('categoria','nombre');
+        return res.json({
+            results: (producto)? [producto] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const productos = await Producto
+                                .find({nombre: regex, estado: true})
+                                .populate('categoria','nombre');
+
+    res.json({
+        resulsts: productos
+    });
+
+}
+
+
+
 
 const buscar = (req, res = response) => {
     const {coleccion, termino} = req.params;
@@ -33,11 +92,11 @@ const buscar = (req, res = response) => {
         case 'usuarios':
             buscarUsuarios(termino,res);
         break;
-        case 'categoria':
-
+        case 'categorias':
+            buscarCategorias(termino,res);
         break;
         case 'productos':
-
+            buscarProductos(termino,res);
         break;
 
         default:
