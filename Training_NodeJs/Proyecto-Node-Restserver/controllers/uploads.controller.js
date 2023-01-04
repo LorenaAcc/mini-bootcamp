a52ea402702b560099eb19b8d98d333516019441
Lorena Martin <lorena.martin@accenture.com>
2023-01-04 11:19:14 -0300

    Borrar imágenes de Cloudinary-archivo example_env

--- Training_NodeJs/Proyecto-Node-Restserver/controllers/uploads.controller.js
+++ Training_NodeJs/Proyecto-Node-Restserver/controllers/uploads.controller.js
@@ -111,7 +111,10 @@ const actualizarImagenCloudinary = async(req, res = response) => {
     //Limpiar imágenes previas
     if(modelo.img){
 
-
+        const nombreArr = modelo.img.split('/');
+        const nombre = nombreArr[nombreArr.length-1];
+        const [public_id] = nombre.split('.');
+        cloudinary.uploader.destroy(public_id);
 
     }
 
