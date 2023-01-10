4afb68d7a7248cee5824dbd1a2276ce8985236a3
Lorena Martin <lorena.martin@accenture.com>
2023-01-10 16:05:10 -0300

    Eliminar registros

--- Training_NodeJs/ts-restserver/controllers/usuarios.controller.ts
+++ Training_NodeJs/ts-restserver/controllers/usuarios.controller.ts
@@ -81,12 +81,20 @@ export const putUsuario = async(req: Request, res: Response) => {
 }
 
 
-export const deleteUsuario = (req: Request, res: Response) => {
+export const deleteUsuario = async(req: Request, res: Response) => {
 
     const {id} = req.params;
+
+    const usuario = await Usuario.findByPk(id);
+    if(!usuario) {
+        return res.status(404).json({
+            msg: 'No existe un usuario con el id ' + id
+        });
+    }
     
-    res.json({
-        msg: 'deleteUsuario',
-        id
-    });
+    await usuario.update({estado: false});
+
+    //await usuario.destroy();
+    
+    res.json(usuario);
 }
--- Training_NodeJs/ts-restserver/dist/controllers/usuarios.controller.js
+++ Training_NodeJs/ts-restserver/dist/controllers/usuarios.controller.js
@@ -78,12 +78,17 @@ const putUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () 
     }
 });
 exports.putUsuario = putUsuario;
-const deleteUsuario = (req, res) => {
+const deleteUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: 'deleteUsuario',
-        id
-    });
-};
+    const usuario = yield usuario_1.default.findByPk(id);
+    if (!usuario) {
+        return res.status(404).json({
+            msg: 'No existe un usuario con el id ' + id
+        });
+    }
+    yield usuario.update({ estado: false });
+    //await usuario.destroy();
+    res.json(usuario);
+});
 exports.deleteUsuario = deleteUsuario;
 //# sourceMappingURL=usuarios.controller.js.map
