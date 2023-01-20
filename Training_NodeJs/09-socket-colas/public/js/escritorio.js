03853d86845f6188501cdf1eb87fdbd141fa9582
Lorena Martin <lorena.martin@accenture.com>
2023-01-20 16:00:27 -0300

    Preparar pantalla de Escritorio

--- Training_NodeJs/09-socket-colas/public/js/escritorio.js
+++ Training_NodeJs/09-socket-colas/public/js/escritorio.js
@@ -1,3 +1,41 @@
+//Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+
+const socket = io();
 
 
-console.log('Escritorio HTML');
+
+socket.on('connect', () => {
+    //botón habilitado
+    btnAtender.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+    //botón deshabilitado
+    btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+    //lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+})
+
+
+btnAtender.addEventListener( 'click', () => {
+
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //     lblNuevoTicket.innerText = ticket;
+    // });
+
+});
