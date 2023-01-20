63e2004588408af09f1e11d8b7474a26caf48624
Lorena Martin <lorena.martin@accenture.com>
2023-01-20 16:48:11 -0300

    Socket: Atender un ticket

--- Training_NodeJs/09-socket-colas/public/js/escritorio.js
+++ Training_NodeJs/09-socket-colas/public/js/escritorio.js
@@ -1,6 +1,9 @@
 //Referencias HTML
 const lblEscritorio = document.querySelector('h1');
 const btnAtender = document.querySelector('button');
+const lblTicket = document.querySelector('small');
+const divAlerta = document.querySelector('.alert');
+
 
 const searchParams = new URLSearchParams(window.location.search);
 
@@ -11,6 +14,8 @@ if (!searchParams.has('escritorio')) {
 
 const escritorio = searchParams.get('escritorio');
 lblEscritorio.innerText = escritorio;
+
+divAlerta.style.display = 'none';
 
 const socket = io();
 
@@ -34,6 +39,14 @@ socket.on('ultimo-ticket', (ultimo) => {
 
 btnAtender.addEventListener( 'click', () => {
 
+    socket.emit('atender-ticket', {escritorio}, ({ok, ticket, msg}) => {
+        if (!ok) {
+            lblTicket.innerText = 'Nadie';
+            return divAlerta.style.display = '';
+        }
+
+        lblTicket.innerText = 'Ticket ' + ticket.numero;
+    });
     // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
     //     lblNuevoTicket.innerText = ticket;
     // });
--- Training_NodeJs/09-socket-colas/sockets/controller.js
+++ Training_NodeJs/09-socket-colas/sockets/controller.js
@@ -15,6 +15,28 @@ const socketController = (socket) => {
         //TODO: Notificar que hay un nuevo ticket pendiente de asignar
     })
 
+    socket.on('atender-ticket', ({escritorio}, callback) => {
+        if (!escritorio) {
+            return callback({
+                ok: false,
+                msj: 'El escritorio es obligatorio'
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket(escritorio);
+        if (!ticket) {
+            callback({
+                ok:false,
+                msg: 'Ya no hay tickets pendientes'
+            });
+        } else {
+            callback({
+                ok:true,
+                ticket
+            });
+        }
+    })
+
 }
 
 
